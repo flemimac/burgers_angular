352855f575758a95bcf9d8eefa18bb52927651a7
flemimac <64695348+flemimac@users.noreply.github.com>
2023-12-12 21:09:19 +0200

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -19,106 +21,13 @@ export class AppComponent {
     phone: ['', Validators.required],
   });
 
-  productsData = [
-    {
-      image: '1.png',
-      title: 'Бургер чеддер & бекон',
-      text: 'Котлета из говядины криспи, булочка, томат, сыр Чеддер, грудинка, лук красный, салат айсбер, майонез, кетчуп, сырный соус',
-      price: 8,
-      basePrice: 8,
-      grams: 360,
-    },
-    {
-      image: '2.png',
-      title: 'BBQ с беконом и курицей',
-      text: 'Булочка бриошь с кунжутом, куриная котлета, сыр чеддер, томат, огурец маринованный, лук маринованный, салат Ромен, бекон, соус BBQ',
-      price: 7,
-      basePrice: 7,
-      grams: 390,
-    },
-    {
-      image: '3.png',
-      title: 'Дабл биф бургер',
-      text: 'Две говяжьи котлеты, сыр чеддер, салат романо, маринованные огурцы, свежий томат, бекон, красный лук, соус бургер, горчица',
-      price: 10,
-      basePrice: 10,
-      grams: 420,
-    },
-    {
-      image: '4.png',
-      title: 'Баварский бургер',
-      text: 'Булочка для бургера, говяжья котлета, красный лук, сыр, охотничья колбаска, соус барбекю, соус сырный, салат айсберг',
-      price: 7,
-      basePrice: 7,
-      grams: 220,
-    },
-    {
-      image: '5.png',
-      title: 'Бекон чизбургер',
-      text: 'Булочка для бургера, говяжья котлета, грудинка, помидор, огурец маринованный, сыр, сырный соус, кетчуп, зелень',
-      price: 8,
-      basePrice: 8,
-      grams: 220,
-    },
-    {
-      image: '6.png',
-      title: 'Индиана бургер',
-      text: 'Булочка для бургера, котлета куриная, грудинка, яйцо, огурец маринованный, криспи лук, кетчуп, соус сырный, горчица, зелень',
-      price: 9,
-      basePrice: 9,
-      grams: 320,
-    },
-    {
-      image: '7.png',
-      title: 'Вегги бургер',
-      text: 'Булочка для бургера, вегетарианская котлета, красный лук, сыр, свежий томат, соус барбекю, соус сырный, салат айсберг',
-      price: 8,
-      basePrice: 8,
-      grams: 280,
-    },
-    {
-      image: '8.png',
-      title: 'Плаксивый Джо',
-      text: 'Булочка для бургера, говяжья котлета, грудинка, помидор, огурец маринованный, красный лук, сыр, перец халапеньо, кетчуп, зелень',
-      price: 7,
-      basePrice: 7,
-      grams: 380,
-    },
-    {
-      image: '9.png',
-      title: 'Двойной чиз бургер',
-      text: 'Булочка для бургера, две говяжьи котлеты, двойной сыр чеддар, огурец маринованный, криспи лук, кетчуп, соус сырный, горчица, зелень',
-      price: 11,
-      basePrice: 11,
-      grams: 400,
-    },
-    {
-      image: '10.png',
-      title: 'Фрешбургер',
-      text: 'Булочка для бургера, говяжья котлета, бекон, сыр чеддар, яйцо, салями, соус барбекю, соус сырный, салат айсберг, свежий томат',
-      price: 9,
-      basePrice: 9,
-      grams: 300,
-    },
-    {
-      image: '11.png',
-      title: 'Цуккини бургер',
-      text: 'Булочка для бургера, вегетарианская котлета из нута, цуккини на гриле, помидор, огурец маринованный, сыр, горчичный соус, кетчуп, зелень',
-      price: 8,
-      basePrice: 8,
-      grams: 320,
-    },
-    {
-      image: '12.png',
-      title: 'Двойной бургер чеддар',
-      text: 'Булочка для бургера, котлета говяжья, грудинка, красный лук, огурец маринованный, томат, кетчуп, двойной сыр чеддар, горчица, зелень',
-      price: 9,
-      basePrice: 9,
-      grams: 360,
-    },
-  ];
+  productsData: any;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private appService: AppService) {}
+
+  ngOnInit() {
+    this.appService.getData().subscribe((data) => (this.productsData = data));
+  }
 
   scrollTo(target: HTMLElement, burger?: any) {
     target.scrollIntoView({ behavior: 'smooth' });
@@ -131,8 +40,15 @@ export class AppComponent {
 
   confirmOrder() {
     if (this.form.valid) {
-      alert('Спасибо за заказ! Мы скоро свяжемся с Вами!');
-      this.form.reset();
+      this.appService.sendOrder(this.form.value).subscribe({
+        next: (response: any) => {
+          alert(response.message);
+          this.form.reset();
+        },
+        error: (response) => {
+          alert(response.error.message);
+        },
+      });
     }
   }
 
